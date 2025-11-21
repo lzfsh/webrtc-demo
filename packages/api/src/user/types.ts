1f422ed2ed227df4a5be56e09d62537f2babeace
lzfsh <59356029+lzfsh@users.noreply.github.com>
2025-11-21 18:56:16 +0800

    types(api): update user type: add UserWithLoginStatus

--- packages/api/src/user/types.ts
+++ packages/api/src/user/types.ts
@@ -1,6 +1,6 @@
 /** 用户连接状态 1: 在线, 0: 离线 */
-export const ConnStatus = Object.freeze({ Online: 1, Offline: 0 })
-export type IConnStatus = (typeof ConnStatus)[keyof typeof ConnStatus]
+export const LoginStatus = Object.freeze({ Online: 1, Offline: 0 })
+export type ILoginStatus = (typeof LoginStatus)[keyof typeof LoginStatus]
 
 export interface User {
   id: number
@@ -25,9 +25,13 @@ export interface RegisterRequest extends Pick<User, 'username' | 'email'> {
 export type RegisterResponse = LoginResponse
 
 export interface ListUserRequest {
-  connStatus?: IConnStatus // 连接状态
+  id?: number // 用户ID
+  email?: string // 邮箱
+  username?: string // 用户名
+  loginStatus?: ILoginStatus // 登录状态
 }
 
-export type ListUserResponse = (User & {
-  connStatus: IConnStatus // 连接状态
-})[]
+export interface UserWithLoginStatus extends User {
+  loginStatus: ILoginStatus // 登录状态
+}
+export type ListUserResponse = UserWithLoginStatus[]
