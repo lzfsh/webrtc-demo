afe6bc1eb989a943bad276255e53d33f65ec2a1e
lzfsh <59356029+lzfsh@users.noreply.github.com>
2025-11-23 01:28:20 +0800

    types(api): change Client to HttpClient

--- packages/api/src/auth/api.ts
+++ packages/api/src/auth/api.ts
@@ -1,15 +1,15 @@
-import type { Client, Response } from '../types'
+import type { HttpClient, Response } from '../types'
 import type { LoginRequest, LoginResponse, RegisterRequest, RegisterResponse } from './types'
 
-export function AuthClient(client: Client) {
+export function AuthClient<C extends object>(client: HttpClient<C>) {
   const LoginURL = '/api/auth/login'
-  const login = <C = any>(req: LoginRequest, conf?: C) => {
-    return client.post<LoginRequest, Response<LoginResponse>, C>(LoginURL, req, conf)
+  const login = (req: LoginRequest, conf?: C) => {
+    return client.post<LoginRequest, Response<LoginResponse>>(LoginURL, req, conf)
   }
 
   const RegisterURL = '/api/auth/register'
-  const register = <C = any>(req: RegisterRequest, conf?: C) => {
-    return client.post<RegisterRequest, Response<RegisterResponse>, C>(RegisterURL, req, conf)
+  const register = (req: RegisterRequest, conf?: C) => {
+    return client.post<RegisterRequest, Response<RegisterResponse>>(RegisterURL, req, conf)
   }
 
   return { LoginURL, login, RegisterURL, register } as const
--- packages/api/src/user/api.ts
+++ packages/api/src/user/api.ts
@@ -1,15 +1,15 @@
-import type { Client, Response } from '../types'
+import type { HttpClient, Response } from '../types'
 import type { GetUserResponse, ListUserRequest, ListUserResponse } from './types'
 
-export function UserClient(client: Client) {
+export function UserClient<C extends object>(client: HttpClient) {
   const GetUserURL = '/api/user/profile'
-  const getUser = <C = any>(conf?: C) => {
-    return client.get<Response<GetUserResponse>, C>(GetUserURL, conf)
+  const getUser = (conf?: C) => {
+    return client.get<Response<GetUserResponse>>(GetUserURL, conf)
   }
 
   const ListUserURL = '/api/user/list'
-  const listUser = <C = any>(req: ListUserRequest, conf?: C) => {
-    return client.post<ListUserRequest, Response<ListUserResponse>, C>(ListUserURL, req, conf)
+  const listUser = (req: ListUserRequest, conf?: C) => {
+    return client.post<ListUserRequest, Response<ListUserResponse>>(ListUserURL, req, conf)
   }
 
   return { GetUserURL, getUser, ListUserURL, listUser } as const
