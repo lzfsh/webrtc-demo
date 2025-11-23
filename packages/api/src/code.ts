d44b554d6399057034dfca59663e095355050d2e
lzfsh <59356029+lzfsh@users.noreply.github.com>
2025-11-23 20:03:40 +0800

    style(api): code utils

--- packages/api/src/code.ts
+++ packages/api/src/code.ts
@@ -1,3 +1,5 @@
+import type { Response } from './types'
+
 export const Code = Object.freeze({
   Ok: 200,
   MultipleChoices: 300,
@@ -11,3 +13,11 @@ export const Code = Object.freeze({
   ServiceUnavailable: 503,
   GatewayTimeout: 504,
 })
+
+export function isSuccess<T>(resp: Response<T>): boolean {
+  return resp.code >= Code.Ok && resp.code < Code.MultipleChoices
+}
+
+export function isFailure<T>(resp: Response<T>): boolean {
+  return resp.code < Code.Ok || resp.code >= Code.MultipleChoices
+}
