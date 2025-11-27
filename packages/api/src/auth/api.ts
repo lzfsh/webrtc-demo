eee2b92503cc9b8fdbf3bcd406493e4322de10af
lzfsh <59356029+lzfsh@users.noreply.github.com>
2025-11-27 21:21:22 +0800

    feat(api): auth api: logout

--- packages/api/src/auth/api.ts
+++ packages/api/src/auth/api.ts
@@ -3,6 +3,7 @@ import type { LoginRequest, LoginResponse, RegisterRequest, RegisterResponse } f
 
 export const LOGIN_URL = '/api/auth/login'
 export const REGISTER_URL = '/api/auth/register'
+export const LOGOUT_URL = '/api/auth/logout'
 
 export function AuthClient<C extends object>(client: HttpClient<C>) {
   const login = (req: LoginRequest, conf?: C) => {
@@ -13,5 +14,9 @@ export function AuthClient<C extends object>(client: HttpClient<C>) {
     return client.post<RegisterRequest, Response<RegisterResponse>>(REGISTER_URL, req, conf)
   }
 
-  return { LOGIN_URL, login, REGISTER_URL, register } as const
+  const logout = (conf?: C) => {
+    return client.post(LOGOUT_URL, {}, conf)
+  }
+
+  return { LOGIN_URL, login, REGISTER_URL, register, LOGOUT_URL, logout } as const
 }
